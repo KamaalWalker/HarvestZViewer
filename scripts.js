137d0379dd28bb75f11a7027d28fac234a69eb52
KamaalWalker <32143331+KamaalWalker@users.noreply.github.com>
2024-06-24 20:48:11 -0700

    Added to list
    Added more names on list

--- scripts.js
+++ scripts.js
@@ -17,6 +17,15 @@ document.addEventListener('DOMContentLoaded', () => {
             { name: 'Dutch', url: 'https://www.youtube.com/embed/QON5EM1xhoY' },
             { name: 'Lilit Highlight', url: 'https://streamable.com/0x9ro7' },
             { name: 'War Highlight', url: 'https://youtube.com/clip/UgkxmIeqP0H6Y0e5S-3IJMYgbe81cWZjc-Nm?feature=shared' },
+            { name: 'Harvest A', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest B', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest C', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest D', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest E', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest F', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest G', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest H', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
+            { name: 'Harvest I', url: 'https://www.youtube.com/embed/A7S_G8Z23_M' },
         ];
 
         perspectives.forEach(perspective => {
